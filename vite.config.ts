ba04025de2fc0c96e752c6c2c499427f827f5164
Yulia Khavaeva <juliahavaeva7@gmail.com>
2024-09-27 20:37:51 +0300

    test: move test setup in tests directory

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+    },
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTest.ts',
+    setupFiles: './tests/config/setupTest.ts',
     include: ['src/**/*.test.{ts,tsx}'],
     coverage: {
       reporter: ['text', 'json', 'html'],
